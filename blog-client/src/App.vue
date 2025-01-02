a7106eb5f73f77e994feb6c27d30bd8e351806d2
Christian Andonov <codepro@gmx.com>
2025-01-02 18:28:34 +0000

    Listing posts on the Home page and front-end adjustments

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -1,3 +1,5 @@
 <template>
-  <router-view />
+  <div class="max-w-4xl mx-auto py-20 px4 sm:px-6 lg:px-8">
+    <router-view />
+  </div>
 </template>
--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -1,7 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/app.css'
-import router from "./router";
+import router from './router'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://blog-project.test'
 
 createApp(App)
   .use(router)
