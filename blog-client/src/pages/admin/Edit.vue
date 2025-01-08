179e62a9b3fe62d9a68ddcf18dabb4c7a34ed0f5
Christian Andonov <codepro@gmx.com>
2025-01-08 22:35:05 +0000

    Watch the post for edits

--- blog-client/src/pages/admin/Edit.vue
+++ blog-client/src/pages/admin/Edit.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    {{ post }}
+    <div>
+      <textarea v-model="post.title" class="w-full text-center text-4xl lg:text-6xl leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0">
+      </textarea>
+    </div>
   </div>
 </template>
 
 <script>
 import useAdminPosts from '../../api/useAdminPosts'
-import { onMounted } from 'vue'
+import { onMounted, watch } from 'vue'
+import _ from 'lodash' // cloneDeep below and debounce are imported from here
 
 export default {
   props: {
@@ -17,7 +21,16 @@ export default {
   },
   setup(props)  {
     const { post, fetchPost } = useAdminPosts()
-    onMounted(() => fetchPost(props.slug))
+    const updatePost = () => {
+      console.log('update')
+    }
+
+    onMounted(async () => {
+      await fetchPost(props.slug)
+      watch(() => _.cloneDeep(post), _.debounce(() => {
+        updatePost()
+      }, 500))
+    })
     return {
       post
     }
