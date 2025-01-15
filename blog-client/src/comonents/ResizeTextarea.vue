0ff3deba39b49debe5375d0af8d36f467d6dae83
Christian Andonov <codepro@gmx.com>
2025-01-15 17:27:52 +0000

    Post title height auto adjust

--- blog-client/src/comonents/ResizeTextarea.vue
+++ blog-client/src/comonents/ResizeTextarea.vue
@@ -1,14 +1,25 @@
 /* In renderless components, we don't use the template tags */
 <script>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 export default  {
   setup(props, context) {
-    const resize = () => {
-      console.log('resized')
+    const el = ref(null)
+
+    const resizeTarget = (target) => {
+      target.style.height = 'auto'
+      target.style.height = `${target.scrollHeight}px`
+    }
+    const resize = (e) => {
+      resizeTarget(e.target)
     }
 
+    onMounted(() => {
+      resizeTarget(el.value)
+    })
+
     return () => context.slots.default({
-      resize
+      resize,
+      el
     })
   },
 }
--- blog-client/src/pages/admin/Edit.vue
+++ blog-client/src/pages/admin/Edit.vue
@@ -1,10 +1,8 @@
 <template>
   <div>
-    <ResizeTextarea>
-      <template v-slot:default="{ resize }">
-        {{ greeting.value }}
-        <textarea v-model="post.title" @input="resize" class="w-full text-center text-4xl lg:text-6xl leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0">
-        </textarea>
+    <ResizeTextarea v-if="post.title">
+      <template v-slot:default="{ resize, el }">
+        <textarea :ref="el" v-model="post.title" @input="resize" class="w-full text-center text-4xl lg:text-6xl leading-10 font-extrabold tracking-tight text-gray-900 border-none focus:ring-0 resize-none p-0" rows="1"></textarea>
       </template>
     </ResizeTextarea>
   </div>
